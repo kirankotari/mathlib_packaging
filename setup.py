40c9ceccde0ca68f1037c61e9f95a836799c291e
kkotari <kotarikirankumar@gmail.com>
2018-12-26 16:24:13 +0530

    Adding tox testing setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+	long_description = f.read()
+
+setup(
+	name='mathlib_kkotari',
+	version='1.0.0',
+	description='A sample mathlib',
+	long_description = long_description,
+	long_description_content_type = 'text/markdown',
+	url = 'https://mathlib-kkotari.com',
+	author = 'kkotari',
+	author_email='kotarikirankumar@gmail.com',
+	classifiers = [ 
+		'Development Status :: 3 - Alpha',
+		'Intended Audience :: Developers',
+		'Topic :: Software Development :: Build Tools',
+		'License :: OSI Approved :: MIT License', 
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3.x',
+		],
+	keywords = 'mathlib kkotari',
+	packages = find_packages(exclude=['tests']),
+)
